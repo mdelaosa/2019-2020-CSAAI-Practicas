434be5f9673613ddaae01bba1e7b7ee052d22d09
mdelaosa <43533564+mdelaosa@users.noreply.github.com>
2020-03-19 09:16:45 +0100

    L7 - 02

---
+++ P3/L7/pong-02.js
@@ -0,0 +1,19 @@
+console.log("Ejecutando JS...");
+
+//-- Obtener el objeto canvas
+const canvas = document.getElementById("canvas");
+
+//-- Sus dimensiones las hemos fijado en el fichero
+//-- HTML. Las imprimimos en la consola
+console.log(`canvas: Anchura: ${canvas.width}, Altura: ${canvas.height}`);
+
+//-- Obtener el contexto para pintar en el canvas
+const ctx = canvas.getContext("2d");
+
+//----- Dibujar la Bola
+ctx.beginPath();
+ctx.fillStyle='white';
+
+//-- x,y, anchura, altura
+ctx.rect(100, 200, 10, 10);
+ctx.fill();
