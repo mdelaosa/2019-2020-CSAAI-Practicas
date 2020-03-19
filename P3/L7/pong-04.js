c7928cfebd703e6b662269a71c3b65278619a5d7
mdelaosa <43533564+mdelaosa@users.noreply.github.com>
2020-03-19 09:24:21 +0100

    L7 - 04

---
+++ P3/L7/pong-04.js
@@ -0,0 +1,48 @@
+console.log("Ejecutando JS...");
+
+//-- Obtener el objeto canvas
+const canvas = document.getElementById("canvas");
+
+//-- Sus dimensiones las hemos fijado en el fichero
+//-- HTML. Las imprimimos en la consola
+console.log(`canvas: Anchura: ${canvas.width}, Altura: ${canvas.height}`);
+
+//-- Obtener el contexto para pintar en el canvas
+const ctx = canvas.getContext("2d");
+
+//----- Dibujar la Bola
+ctx.beginPath();
+ctx.fillStyle='lightblue';
+
+//-- x,y, anchura, altura
+ctx.rect(100, 200, 10, 10);
+ctx.fill();
+
+//------- Dibujar las raquetas
+ctx.beginPath();
+ctx.fillStyle='pink';
+
+//-- Raqueta izquierda
+ctx.rect(50, 100, 10, 50);
+
+//-- Raqueta derecha
+ctx.rect(540, 300, 10, 50);
+
+//-- Pintar!
+ctx.fill();
+
+//--------- Dibujar la red
+ctx.beginPath();
+
+//-- Estilo de la linea: discontinua
+//-- Trazos de 10 pixeles, y 10 de separacion
+ctx.setLineDash([20, 5]);
+ctx.strokeStyle = 'lightgreen';
+ctx.lineWidth = 1;
+//-- Punto superior de la linea. Su coordenada x está en la mitad
+//-- del canvas
+ctx.moveTo(canvas.width/3, 0);
+
+//-- Dibujar hasta el punto inferior
+ctx.lineTo(canvas.width/2, canvas.height);
+ctx.stroke();
