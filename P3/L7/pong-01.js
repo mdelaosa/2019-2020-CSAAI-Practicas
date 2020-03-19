8453ff7e9c04c6fb3be01e5f10bd57c45d866163
mdelaosa <43533564+mdelaosa@users.noreply.github.com>
2020-03-19 09:13:25 +0100

    L7 - 01

---
+++ P3/L7/pong-01.js
@@ -0,0 +1,8 @@
+console.log("Ejecutando JS...");
+
+//-- Obtener el objeto canvas
+const canvas = document.getElementById("canvas");
+
+//-- Sus dimensiones las hemos fijado en el fichero
+//-- HTML. Las imprimimos en la consola
+console.log(`canvas: Anchura: ${canvas.width}, Altura: ${canvas.height}`);
