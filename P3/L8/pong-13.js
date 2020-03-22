2e2ddadb3b5fd73468c73943c842a774707c632a
mdelaosa <43533564+mdelaosa@users.noreply.github.com>
2020-03-22 18:47:47 +0100

    L8 - 05

---
+++ P3/L8/pong-13.js
@@ -0,0 +1,132 @@
+console.log("Ejecutando JS...");
+
+//-- Obtener el objeto canvas
+const canvas = document.getElementById("canvas");
+
+//-- Sus dimensiones las hemos fijado en el fichero
+//-- HTML. Las imprimimos en la consola
+console.log(`canvas: Anchura: ${canvas.width}, Altura: ${canvas.height}`);
+
+//-- Obtener el contexto para pintar en el canvas
+const ctx = canvas.getContext("2d");
+
+//-- Variables para la bola
+let bola_x = 100;
+let bola_y = 200;
+let bola_vx = 0;
+
+//-- Variables para la raqueta izquierda
+let raqI_x = 50;
+let raqI_y = 100;
+let raqI_v = 0;  //-- Velocidad
+
+//-- Pintar todos los objetos en el canvas
+function draw() {
+  //----- Dibujar la Bola
+  ctx.beginPath();
+  ctx.fillStyle='lightblue';
+
+  //-- x,y, anchura, altura
+  ctx.rect(bola_x, bola_y, 10, 10);
+  ctx.fill();
+
+  //------- Dibujar las raquetas
+  ctx.beginPath();
+  ctx.fillStyle='pink';
+
+  //-- Raqueta izquierda
+  ctx.rect(raqI_x, raqI_y, 10, 40);
+
+  //-- Raqueta derecha
+  ctx.rect(540, 300, 10, 50);
+
+  //-- Pintar!
+  ctx.fill();
+
+  //--------- Dibujar la red
+  ctx.beginPath();
+
+  //-- Estilo de la linea: discontinua
+  //-- Trazos de 10 pixeles, y 10 de separacion
+  ctx.setLineDash([20, 5]);
+  ctx.strokeStyle = 'lightgreen';
+  ctx.lineWidth = 2;
+  //-- Punto superior de la linea. Su coordenada x está en la mitad
+  //-- del canvas
+  ctx.moveTo(canvas.width/2, 0);
+
+  //-- Dibujar hasta el punto inferior
+  ctx.lineTo(canvas.width/2, canvas.height);
+  ctx.stroke();
+
+  //------ Dibujar el tanteo
+  ctx.font = "100px Arial";
+  ctx.fillStyle = "#CC99FF";
+  ctx.fillText("0", 200, 80);
+  ctx.fillText("1", 340, 80);
+
+}
+
+//---- Bucle principal de la animación
+function animacion()
+{
+
+  //-- Actualizar las posiciones de los objetos móviles
+
+  //-- Actualizar la raqueta con la velocidad actual
+  raqI_y += raqI_v;
+
+  //-- Comprobar si la bola ha alcanzado el límite derecho
+  //-- Si es así, se cambia de signo la velocidad, para
+  // que "rebote" y vaya en el sentido opuesto
+  if (bola_x >= canvas.width) {
+    //-- Hay colisión. Cambiar el signo de la bola
+    bola_vx = bola_vx * -1;
+  }
+
+  //-- Comprobar si hay colisión con la raqueta izquierda
+  if (bola_x >= raqI_x && bola_x <=(raqI_x+10) &&
+      bola_y >= raqI_y && bola_y <=(raqI_y+40)) {
+    bola_vx = bola_vx * -1;
+  }
+
+  //-- Actualizar coordenada x de la bola, en funcion de
+  //-- su velocidad
+  bola_x += bola_vx;
+
+  //-- Borrar la pantalla
+  ctx.clearRect(0,0, canvas.width, canvas.height);
+
+  //-- Dibujar el nuevo frame
+  draw();
+}
+
+//-- Arrancar la animación
+setInterval(()=>{
+  animacion();
+},16);
+
+//-- Retrollamada de las teclas
+window.onkeydown = (e) => {
+
+  switch (e.key) {
+    case "a":
+      raqI_v = 3;
+      break;
+    case "q":
+      raqI_v = -3;
+      break;
+    case " ":
+      bola_x = 50;
+      bola_vx = 6;
+    default:
+  }
+}
+
+//-- Retrollamada de la liberacion de teclas
+window.onkeyup = (e) => {
+  if (e.key == "a" || e.key == "q"){
+    //-- Quitar velocidad de la raqueta
+    raqI_v = 0;
+  }
+}
