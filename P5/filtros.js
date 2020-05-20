c7f536072bdb209bd04f057afb9a1711f4d053ed
mdelaosa <43533564+mdelaosa@users.noreply.github.com>
2020-05-20 11:36:37 +0200

    .

--- P5/filtros.js
+++ P5/filtros.js
@@ -108,9 +108,10 @@ grises.onclick = () => {
   ctx.drawImage(img, 0,0);
   let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
   let data = imgData.data;
+  var realce = 0;
   for (let i = 0; i < data.length; i+=4) {
-    brillo = (3*data[i] + 4*data[i+1]+ data[i+2])/8
-    data[i] = data[i+1] = data[i+2] = brillo;
+    realce = (3*data[i] + 4*data[i+1]+ data[i+2])/8
+    data[i] = data[i+1] = data[i+2] = realce;
   }
 ctx.putImageData(imgData, 0,0);
 
