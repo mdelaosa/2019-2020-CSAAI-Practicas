bd83383b69333fff3190d8d081488b576a48df8c
mdelaosa <43533564+mdelaosa@users.noreply.github.com>
2020-05-23 21:14:10 +0200

    Prueba máquina de estados

--- P2/calculadora.js
+++ P2/calculadora.js
@@ -10,7 +10,61 @@
  let digit = document.getElementsByClassName("digit");
  let operator = document.getElementsByClassName("operator");
 
- // Digitos
+ //-- Estados de la calculadora
+const ESTADO = {
+  INIT: 0,
+  OP1: 1,
+  OPERATION: 2,
+  OP2_INIT: 3,
+  OP2: 4,
+}
+
+  let estado = ESTADO.INIT;
+
+  //-- Digitos
+  for(i=0; i<digit.length; i++){
+    digit[i].onclick=(ev)=>{
+     number(ev.target.value);
+     console.log(`ESTADO ${estado}`);
+    }
+  }
+
+  //-- Operadores
+  for(i=0; i<operator.length; i++){
+    operator[i].onclick=(ev)=>{
+      operators(ev.target.value);
+      console.log(`ESTADO ${estado}`);
+    }
+  }
+
+  function number(num){
+    //-- Segun el estado hacemos una cosa u otra
+    if (estado == ESTADO.INIT) {
+      display.innerHTML = num;
+      estado = ESTADO.OP1;
+    }else if (estado == ESTADO.OP1){
+      display.innerHTML += num;
+    } else if (estado == ESTADO.OPERATION) {
+      display.innerHTML += num;
+      estado = ESTADO.OP2_INIT;
+    }else if (estado == ESTADO.OP2_INIT) {
+      display.innerHTML +=  num;
+      estado = ESTADO.OP2;
+    }else if (estado == ESTADO.OP2){
+      display.innerHTML += num;
+    }
+  }
+
+  function operators(operat){
+  if (estado != ESTADO.OPERATION) {
+    display.innerHTML += operat;
+    estado = ESTADO.OPERATION;
+  }
+}
+
+
+
+ /*// Digitos
  for (i=0; i<digit.length; i++) {
   digit[i].onclick = (ev) => {
     if (display.innerHTML == "0"){
@@ -31,7 +85,7 @@ for (i=0; i<operator.length; i++){
       display.innerHTML += ev.target.value;
     }
   }
-}
+}*/
 
   // Igual
   equal.onclick = () => {
@@ -40,13 +94,13 @@ for (i=0; i<operator.length; i++){
 
   // Borrar todo
  c.onclick = () => {
-   display.innerHTML = "0";
+   display.innerHTML = "";
  }
 
 // Borrar lo ultimo añadido
  del.onclick = () => {
    if (display.innerHTML == "0"){
-     display.innerHTML = "0";
+     display.innerHTML = "";
    }else{
      display.innerHTML = display.innerHTML.slice(0,-1);
    }
