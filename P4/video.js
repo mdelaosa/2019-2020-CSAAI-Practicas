3ec54550a0f898a81400b2f402dd07300b34b175
mdelaosa <43533564+mdelaosa@users.noreply.github.com>
2020-05-23 12:41:49 +0200

    Videos colocados

---
+++ P4/video.js
@@ -0,0 +1,22 @@
+console.log("Ejecutando JS...");
+
+//----- Obtener elemento de video y configurarlo
+  const video = document.getElementById("video");
+  video.width = 840;  //-- Tamaño de la pantalla de video
+  video.height = 480;
+
+  const video1 = document.getElementById("video1")
+  video1.src = "https://github.com/mdelaosa/videosP4CSAAI/raw/master/present.mp4"
+  video1.width = 300;  //-- Tamaño de la pantalla de video
+
+  const video2 = document.getElementById("video2")
+  video2.src = "https://github.com/mdelaosa/videosP4CSAAI/raw/master/Beyonce.mp4"
+  video2.width = 300;  //-- Tamaño de la pantalla de video
+
+  const video3 = document.getElementById("video3")
+  video3.src = "https://github.com/mdelaosa/videosP4CSAAI/raw/master/million%20dollars.mp4"
+  video3.width = 300;  //-- Tamaño de la pantalla de video
+
+  const imgprueba = document.getElementById("imgprueba");
+  imgprueba.src = "https://raw.githubusercontent.com/mdelaosa/videosP4CSAAI/master/prueba.jpeg";
+  imagen.width=300;
